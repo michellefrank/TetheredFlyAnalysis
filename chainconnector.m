f044a659310722ebbd6c57d74f64e4eb7a98a3ee
michellefrank <michelle.frank05@gmail.com>
2015-08-12 12:44:15 -0400

    Upgraded processing
    New features include progress bar to track recording progress. Updates
    by S. Zhang.

---
+++ chainconnector.m
@@ -0,0 +1,47 @@
+function [ chainmat_c ] = chainconnector( chainmat , maxgap)
+%chainconnector connects chains that are broken by small (user-defined)
+%number of entries. Default gap is 1.
+%   [ chainmat_c ] = chainconnector( chainmat , maxgap)
+
+if nargin < 2
+    maxgap = 1;
+end
+
+
+% Calculate where the initial chains end
+chainends = chainmat(1:end-1,1) + chainmat(1:end-1,2) - 1;
+
+% Figure out which chain gaps are toleratable
+chainjoints = (chainmat(2:end,1) - chainends) <= (maxgap + 1);
+
+% Find a chainmat of the toleratable gaps
+chainjoints_chains = chainfinder(chainjoints);
+
+% Find the final number of chains
+n_chains = size(chainjoints_chains, 1);
+
+% Prime the new chain mat
+chainmat_c = zeros(n_chains, 2);
+
+for i = 1 : n_chains
+    % Find where this chain starts
+    chainmat_c (i, 1) = chainmat(chainjoints_chains(i,1),1);
+    
+    % Find how long this chain is
+    chainmat_c(i,2) = sum(chainmat(chainjoints_chains(i,1):...
+        chainjoints_chains(i,1)+chainjoints_chains(i,2),2))...
+        + (chainjoints_chains(i,2)+1) * maxgap;
+end
+
+% Label connected joined chains
+conn_chains = [0;chainjoints];
+conn_chains(chainjoints_chains(:,1)) = 1;
+
+% Keep unjoined chains
+unjoined_chains = chainmat(conn_chains==0,:);
+
+% Combine joined and unjoined chains, and sort
+chainmat_c = sortrows([chainmat_c;unjoined_chains]);
+
+
+end
--- flypix_avi.m
+++ flypix_avi.m
@@ -9,10 +9,7 @@ firstframe2load = 1;
 RGBchannel = 1;
 
 % Choose 1 if don't want to see the progress of processing
-quietmode = 1;
-
-% Choose 1 if using batch analysis
-%batchmode = 1;
+quietmode = 0;
 
 %% Load video
 % Specify video name and path
@@ -68,6 +65,8 @@ Vidstack = uint8( zeros(cropindices(4), cropindices(3), nframe2load));
 % Use progress bar if needed
 if quietmode==0
     dispbar=waitbar(0,'Loading Data Video');
+else
+    textprogressbar('Processing: ');
 end
 
 % Load it!
@@ -92,11 +91,15 @@ for i = firstframe2load : frames2skip : nVidFrame
         
         % Update the waitbar
         waitbar(i/nVidFrame,dispbar)
+    else
+        textprogressbar(i/(nVidFrame - firstframe2load)*100);
     end
 end
 
 if quietmode==0
     close(dispbar)
+else
+    textprogressbar('Done!');
 end
 
 %% Calculate pixel subtration
@@ -105,3 +108,6 @@ Pixeldiff = squeeze(sum(sum(abs(diff(Vidstack, 1, 3)), 1), 2));
 plot((1+1/targetfps):1/targetfps:nframe2load,Pixeldiff)
 xlabel('Time(s)')
 ylabel('Pixel Difference')
+
+% Save data
+save(fullfile(path,'Processed data',[filename(1:end-4),'.mat']))
