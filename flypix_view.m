874a0d0510ea5f08b48316ec0aa24286edb35ae6
xzhang03 <x.stephen.zhang@gmail.com>
2015-08-16 17:54:25 -0400

    updated flypix_view

--- flypix_view.m
+++ flypix_view.m
@@ -1,6 +1,12 @@
 %% Parameters
-
+% FPS when viewing the video
 ViewingFPS = 20;
+
+% Threshold for the intensity of the pixel subtraction
+pixthresh = 500;
+
+% Where the circle will be drawn if the pixel subtraction is subthreshold
+circle_position = [size(Vidstack,2) - 20, 20, 10]; %[x,y,rad]
 
 %% Obstain start and end frames
 
@@ -20,10 +26,15 @@ for i = startframe : endframe
     % Write the number (results in an RGB frame)
     tempim = insertText(Vidstack(:,:,i)*255, [10 10], num2str(Pixeldiff(i-1)));
     
+    % Write a circle if below threshold
+    if Pixeldiff(i-1) < pixthresh
+        tempim = insertShape(tempim, 'FilledCircle', circle_position, 'Color', [255 , 0, 0]);
+    end
+    
     % Only use 1 channel from the RGB frame
     newVidstack(:,:,i-startframe+1) = tempim(:,:,1);
 end
 textprogressbar('Done!');
 
 %% Play the video
-implay(newVidstack, Viewing FPS)
+implay(newVidstack, ViewingFPS)
