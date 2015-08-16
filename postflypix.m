fc06fc20bbaaed40982768891ecb43b05f70ba2e
michellefrank <michelle.frank05@gmail.com>
2015-08-16 19:32:41 -0400

    Added new features to postflypix.m
    Added new plot: histogram containing distribution of rest bout
    durations.
    
    Also added auto-save for graphs.

--- postflypix.m
+++ postflypix.m
@@ -1,9 +1,9 @@
 %% Parameters
 % Define the threshold
-pixthresh = 6;
+pixthresh = 500;
 
 % Define minimal chain length
-min_chainlength = 180;
+min_chainlength = 300;
 
 % Deine the max number of entries tolerated by the program
 maxgap = 1;
@@ -19,7 +19,7 @@ chainmat = chainfinder(Pixeldiff <= pixthresh);
 chainmat = chainconnector(chainmat,maxgap);
 
 % Trim the chain
-chainmat = chainmat(chainmat(:,2) >= min_chainlength,:);
+chainmat_trim = chainmat(chainmat(:,2) >= min_chainlength,:);
 
 %% Plotting
 % Find out the number of frames
@@ -42,22 +42,55 @@ ylim = get(gca,'YLim');
 % Add text and lines to signify chains
 hold on
 
-for i = 1 : size(chainmat,1)
+for i = 1 : size(chainmat_trim,1)
     % Draw lines
-    line([chainmat(i,1),chainmat(i,1)+chainmat(i,2)-1]/fps/60/60,...
+    line([chainmat_trim(i,1),chainmat_trim(i,1)+chainmat_trim(i,2)-1]/fps/60/60,...
         [pixthresh,pixthresh],'Color',[0.93,0.13,0.14],...
         'LineWidth',5);
     
     % Write when each chain starts
     if mod(i,2) ~= 0
-        text(chainmat(i,1)/fps/60/60, 0.9*ylim(2),...
-        num2str(round(chainmat(i,1)*fps/60)), 'Color', [0.93,0.13,0.14],...
+        text(chainmat_trim(i,1)/fps/60/60, 0.9*ylim(2),...
+        num2str(round(chainmat_trim(i,1)*fps/60)), 'Color', [0.93,0.13,0.14],...
         'FontSize', 11);
     else
-        text(chainmat(i,1)/fps/60/60, 0.85*ylim(2),...
-        num2str(round(chainmat(i,1)*fps/60)), 'Color', [0.93,0.13,0.14],...
+        text(chainmat_trim(i,1)/fps/60/60, 0.85*ylim(2),...
+        num2str(round(chainmat_trim(i,1)*fps/60)), 'Color', [0.93,0.13,0.14],...
         'FontSize', 11);
     end
 end
 
 hold off
+
+% Save the figure
+savefig(gcf, fullfile(path, [filename(1:end-4), '-rest_bouts.fig']));
+
+%% Plot histogram of rest bout durations
+
+% Filter out bouts with duration shorter than 1 min
+chainmat2 = chainmat(chainmat(:,2) > 60,2);
+
+% Convert units to minutes
+chainmat2 = chainmat2/60;
+
+% Plot the histogram of rest bout durations
+hist(chainmat2, 20);
+set(gcf, 'Color', [1 1 1]);
+xlabel('Minutes', 'fontweight', 'bold');
+ylabel('Counts', 'fontweight', 'bold');
+title('Rest bout durations', 'fontweight', 'bold');
+
+% Save the file
+savefig(gcf, fullfile(path, [filename(1:end-4), '-rest_bouts.fig']));
+
+
+%% Print out summary info
+
+disp('Mean length of rest bouts (>1 min): ');
+disp(mean(chainmat2));
+
+disp('Number of sleep bouts (>5 min): ');
+disp(length(chainmat_trim));
+
+disp('Mean length of sleep bouts (> 5 min): ');
+disp(mean(chainmat_trim(:,2))/60);
