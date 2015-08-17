05683cf5d8210a7e740d490964ea4cbdbeba62d0
xzhang03 <x.stephen.zhang@gmail.com>
2015-08-16 20:33:22 -0400

    Integrate sleepvector from postflypix

--- flypix_view.m
+++ flypix_view.m
@@ -2,9 +2,6 @@
 % FPS when viewing the video
 ViewingFPS = 20;
 
-% Threshold for the intensity of the pixel subtraction
-pixthresh = 500;
-
 % Where the circle will be drawn if the pixel subtraction is subthreshold
 circle_position = [size(Vidstack,2) - 20, 20, 10]; %[x,y,rad]
 
@@ -15,6 +12,9 @@ startframe = input('Enter start frame =');
 endframe = input('Enter end frame =');
 
 %% Stamp in the pixel subtraction results
+% Write out the sleep chain
+sleepchain = chainwritter(chainmat);
+
 newVidstack = Vidstack(:,:,startframe:endframe);
 
 textprogressbar('Processing: ');
@@ -26,8 +26,8 @@ for i = startframe : endframe
     % Write the number (results in an RGB frame)
     tempim = insertText(Vidstack(:,:,i)*255, [10 10], num2str(Pixeldiff(i-1)));
     
-    % Write a circle if below threshold
-    if Pixeldiff(i-1) < pixthresh
+    % Write a circle if sleeping
+    if sleepchain(i-1) == 1
         tempim = insertShape(tempim, 'FilledCircle', circle_position, 'Color', [255 , 0, 0]);
     end
     
